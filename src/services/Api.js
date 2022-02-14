b1ecbf56118fba98e9d571911d4131f775b9ffca
Santtu Lehtokari <35769108+S1nd5@users.noreply.github.com>
2022-02-14 10:12:15 +0200

    Verkkopyynnöt axioksella, Login,Register lomakkeiden muokkausta

---
+++ src/services/Api.js
@@ -0,0 +1,41 @@
+/* Uusi axios-instanssi */
+const axios = require("axios");
+const instance = axios.create({
+  baseURL: "https://httpbin.org/",
+  timeout: 1000,
+  headers: { "X-Custom-Header": "foobar" },
+});
+
+/* Metodit */
+
+/* Posts login payload to /api/login */
+
+const login = (data) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .post("/post", data)
+      .then(function (response) {
+        return resolve(response.data);
+      })
+      .catch(function (error) {
+        return resolve(error);
+      });
+  });
+};
+
+/* Posts register payload to /api/register */
+
+const registerUser = (data) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .post("/login", data)
+      .then(function (response) {
+        return resolve(response.data);
+      })
+      .catch(function (error) {
+        return resolve(error);
+      });
+  });
+};
+
+module.exports = { login, registerUser };
