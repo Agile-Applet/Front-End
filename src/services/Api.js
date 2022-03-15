d13e1f20648f47033f542771f325fac786c933fb
Santtu Lehtokari <35769108+S1nd5@users.noreply.github.com>
2022-03-15 02:55:50 +0200

    Socket ja API muuttujat .env filussa

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,8 +1,8 @@
 /* Uusi axios-instanssi */
 const axios = require("axios");
 const instance = axios.create({
-  baseURL: process.env.API_BASE_STRING || 'https://container-service-1.1bm12m42tdcru.eu-north-1.cs.amazonlightsail.com',
-  timeout: 1000,
+  baseURL: process.env.API_BASE_STRING,
+  timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
   validateStatus: () => true,
 });
